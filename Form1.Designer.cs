1e8177f89f5deb21b87bc043b8c5584c4c731e61
look <blueskins@gmail.com>
2020-07-03 11:04:26 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace TirStu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textAge = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 124);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.Size = new System.Drawing.Size(750, 420);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 616);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textID);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textAge);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(868, 124);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 420);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(88, 189);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(120, 35);
+            this.textName.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 352);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 24);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Age";
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(88, 37);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(120, 35);
+            this.textID.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Name";
+            // 
+            // textAge
+            // 
+            this.textAge.Location = new System.Drawing.Point(88, 349);
+            this.textAge.Name = "textAge";
+            this.textAge.Size = new System.Drawing.Size(120, 35);
+            this.textAge.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ID";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 616);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 60);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(626, 616);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 60);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Del";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 863);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textAge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TriStu.BLL;
+using TriStu.Models;
+
+namespace TirStu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StuManager stu = new StuManager();
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = stu.GetStudents();
+        }
+
+        
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student newStu = new Student();
+            newStu.ID = int.Parse(textID.Text);
+            newStu.Name = textName.Text;
+            newStu.Age = int.Parse(textAge.Text);
+            if (stu.AddStudent(newStu))
+            {
+                MessageBox.Show("added successful");
+                dataGridView1.DataSource = stu.GetStudents();
+            }
+            else
+            {
+                MessageBox.Show("added failed!");
+            }
+            
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count <= 0)
+            {
+                MessageBox.Show("please select one line");
+                return;
+            }
+
+            Student newStu = new Student();
+            newStu.ID = int.Parse(dataGridView1.SelectedRows[0].Cells[0].Value.ToString());
+            newStu.Name = textName.Text;
+            newStu.Age = int.Parse(textAge.Text);
+            if (stu.UpdataStudent(newStu))
+            {
+                MessageBox.Show("editted successfully");
+                dataGridView1.DataSource = stu.GetStudents();
+            }
+            else
+            {
+                MessageBox.Show("ediited failed");
+            }
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //判断是否选中dataGridView1里的一行
+            if (dataGridView1.SelectedRows.Count <= 0)
+            {
+                MessageBox.Show("请选择一行进行操作");
+                return;
+            }
+            textID.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textName.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            textAge.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id;
+            if (dataGridView1.SelectedRows.Count <= 0)
+            {
+                MessageBox.Show("please select one line");
+                return;
+            }
+
+            id = int.Parse(dataGridView1.SelectedRows[0].Cells[0].Value.ToString());
+
+            if (stu.DelStudent(id)){
+                MessageBox.Show("deleted sucessfully");
+                dataGridView1.DataSource = stu.GetStudents();
+                textID.Text = "";
+                textName.Text = "";
+                textAge.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("delete failed!");
+            }
+
+        }
+    }
+}
